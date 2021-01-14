641ea24112eee0cd98a39afda7ef8d3983b81d46
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2021-01-14 10:26:34 +0400

    Updates

--- static/js/main.8adf2ab3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpquiz-app"]=this["webpackJsonpquiz-app"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n(0),p=n.n(i),r=n(3),s=function(){return Object(c.jsx)("div",{children:"Quiz App"})};n.n(r).a.render(Object(c.jsx)(p.a.StrictMode,{children:Object(c.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.8adf2ab3.chunk.js.map
